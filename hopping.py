998dece6ce5e7e2dd1d1b48074a3fe8eabbddcff
Pratik Dhuvad <pratikdhuvad@gmail.com>
2018-11-05 21:16:19 -0500

    Update hopping.py

--- hopping.py
+++ hopping.py
@@ -29,8 +29,8 @@ def reset_fun(arr_len):
     return go_to_next 
     
     
-    for arr_len in range(3,16):
-        count=0 # reset global variable
-        go_to_next = reset_fun(arr_len)
-        go_to_next(0)
-        print("N=", arr_len, "P=", count)
+for arr_len in range(3,16):
+    count=0 # reset global variable
+    go_to_next = reset_fun(arr_len)
+    go_to_next(0)
+    print("N=", arr_len, "P=", count)
